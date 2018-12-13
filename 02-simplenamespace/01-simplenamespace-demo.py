7fcbd3722b81da04036a751aeca08e4fcd9eafc5
Guillaume 'Vermeille' Sanchez <guillaume.v.sanchez@gmail.com>
2018-12-13 17:08:18 +0100

    some ops

--- 02-simplenamespace/01-simplenamespace-demo.py
+++ 02-simplenamespace/01-simplenamespace-demo.py
@@ -1,5 +1,6 @@
 from types import SimpleNamespace
 
+
 def vec(x, y):
     u = SimpleNamespace()
     u.x = x
@@ -7,5 +8,31 @@ def vec(x, y):
     return u
 
 
-u = vec(3, 4)
-print(u)
+def vec_add(u, v):
+    return vec(u.x + v.x, u.y + v.y)
+
+
+def vec_sub(u, v):
+    return vec(u.x - v.x, u.y - v.y)
+
+
+def vec_scale(u, s):
+    return vec(s * u.x, s * u.y)
+
+
+def vec_dot(u, v):
+    return u.x * v.x + u.y * v.y
+
+
+def vec_sqlen(u):
+    return u.x * u.x + u.y * u.y
+
+
+def vec_len(u):
+    return vec_sqlen(u) ** 0.5
+
+
+a = vec(3, 4)
+b = vec(10, 10)
+c = vec_add(a, b)
+print(c)
