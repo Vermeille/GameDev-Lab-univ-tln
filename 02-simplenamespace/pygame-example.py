869d9cdc39dcc1b840ba51f4555acf704f6104a6
Guillaume 'Vermeille' Sanchez <guillaume.v.sanchez@gmail.com>
2018-12-13 17:50:22 +0100

    pygame keyboard!

---
+++ 02-simplenamespace/pygame-example.py
@@ -0,0 +1,26 @@
+import pygame
+import time
+
+pygame.init()
+
+W = 800
+H = 600
+screen = pygame.display.set_mode((W, H))
+
+x = 400
+while True:
+    screen.fill((0, 0, 0))
+
+    pygame.event.pump()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        x += 3
+
+    pygame.draw.circle(
+            screen,
+            (255, 255, 255),
+            (x, 300),
+            50)
+
+    pygame.display.flip()
+    time.sleep(1 / 60)
